adacc01cff51837231502348fff6e12be622e62a
srinidhisg88 <srinidhisg1@gmail.com>
2024-10-26 01:49:21 +0530

    small fix

--- routes/stamps.route.js
+++ routes/stamps.route.js
@@ -2,7 +2,7 @@ import express from "express"
 import { authenticate } from "../middlewares/auth.middleware.js"
 import { createStamp,getAllStamps,getStampsByCategory,getStamp} from "../controllers/stamp.controller.js"
 
-import { upload } from '../middlewares/multer.middleware.js';
+import  upload  from '../middlewares/multer.middleware.js';
 const router=express.Router()
 router.post('/createStamp',upload.single('image'),authenticate,createStamp)
 router.get('/:stampId',authenticate,getStamp)
